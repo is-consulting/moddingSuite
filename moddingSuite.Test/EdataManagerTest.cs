b8b9e31a78709c54dcc2320a06863fbd55595c9e
Remi C <bobleboucher@gmail.com>
2021-06-10 20:42:46 -0400

    Added tests to read red dragon & airland battle game date.

---
+++ moddingSuite.Test/EdataManagerTest.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using moddingSuite.BL;
+using System.IO;
+using System.Linq;
+
+namespace moddingSuite.Test
+{
+    [TestClass]
+    public class EdataManagerTest: BaseTests
+    {
+
+        [DataTestMethod]
+        [DataRow("/48574/Data")]
+        [DataRow("/48574/DataMap")]
+        [DataRow("/48574/NDF_NotFinal")]
+        [DataRow("/48574/NDF_Win")]
+        [DataRow("/48574/ZZ_1")]
+        [DataRow("/48574/ZZ_2")]
+        [DataRow("/48574/ZZ_3a")]
+        [DataRow("/48574/ZZ_3b")]
+        [DataRow("/48574/ZZ_4")]
+        [DataRow("/48574/ZZ_NotFinal")]
+        [DataRow("/48574/ZZ_Win")]
+        [DataRow("/49125/NDF_Win")]
+        [DataRow("/49964/NDF_Win")]
+        public void CanParseRedDragonHeader(string path)
+        {
+            var sut = new EdataManager($"{RedDragonGameDataPath}{path}.dat");
+            sut.ParseEdataFile();
+            sut.Files.Should().NotBeEmpty();
+        }
+
+        [DataTestMethod]
+        [DataRow("/49125/NDF_Win")]
+        [DataRow("/49964/NDF_Win")]
+        [DataRow("/48574/NDF_Win")]
+        public void CanReadRawData(string path)
+        {
+            var manager = new EdataManager($"{RedDragonGameDataPath}{path}.dat");
+            manager.ParseEdataFile();
+
+            var config = manager.Files.First(f => f.Path == @"pc\ndf\nonpatchable\config.ndfbin");
+            var bytes = manager.GetRawData(config);
+            bytes.Should().NotBeEmpty();
+        }
+
+        [TestMethod]
+        public void CanParseAirLandBattleHeader()
+        {
+            var files = Directory.EnumerateFiles(AirLandGameDataPath, "*.dat", SearchOption.AllDirectories);
+            var count = 0;
+            foreach(var file in files)
+            {
+                var sut = new EdataManager(file);
+                sut.ParseEdataFile();
+                sut.Header.Magic.Should().BeGreaterThan(0);
+                count++;
+            }
+            count.Should().BeGreaterThan(0);
+        }
+    }
+}
