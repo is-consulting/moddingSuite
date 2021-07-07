e6f6c4f39a9a625c204e0e763748bd836dcd20ee
Remi C <bobleboucher@gmail.com>
2021-07-07 00:17:10 -0400

    Use Nerdbank.GitVersioning for versioning.

--- moddingSuite/ViewModel/About/AboutViewModel.cs
+++ moddingSuite/ViewModel/About/AboutViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using moddingSuite.ViewModel.Base;
+using System.Reflection;
 
 namespace moddingSuite.ViewModel.About
 {
     public class AboutViewModel : ViewModelBase
     {
+        public string Version => Assembly.GetExecutingAssembly().GetName().Version.ToString(); 
     }
 }
